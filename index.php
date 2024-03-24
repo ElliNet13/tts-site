f882eefd42cec9d0895e16cf69c40e23d1305af8
ElliNet13 <elliotmoose4@gmail.com>
2024-03-24 18:02:42 +0000

    Remove replit thing

--- index.php
+++ index.php
@@ -28,6 +28,5 @@
   <div id="run"></div>
   <br><button onclick="shell()">Create TTS</button>
   <br><input id="text" type="text">
-  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
 </body>
 </html>
